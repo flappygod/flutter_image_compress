60e5cd07ecf5ce0706ff3295f0addb129fb6c892
lijunlin <327603258@qq.com>
2021-03-17 10:37:58 +0800

    增加图片保存到本地地址

--- lib/flutterimagecompress.dart
+++ lib/flutterimagecompress.dart
@@ -9,7 +9,7 @@ class Flutterimagecompress {
   static const MethodChannel _channel = const MethodChannel('flutterimagecompress');
 
   //调用系统压缩图片
-  static Future<String> compressImageToSavePath(String path, String savePath, int quality, int maxWidth, int maxHeight) async {
+  static Future<String?> compressImageToSavePath(String path, String savePath, int quality, int maxWidth, int maxHeight) async {
     //剔除file部分
     String nowerPath = path;
     //地址
@@ -17,13 +17,13 @@ class Flutterimagecompress {
       nowerPath = nowerPath.replaceAll("file://", "");
     }
     //调用原生压缩图像
-    final String ret = await _channel.invokeMethod('compressImage',
+    final String? ret = await _channel.invokeMethod('compressImage',
         {"path": nowerPath, "savePath": savePath, "quality": quality.toString(), "maxWidth": maxWidth.toString(), "maxHeight": maxHeight.toString()});
     return ret;
   }
 
   //调用系统压缩图片
-  static Future<String> compressImage(String path, int quality, int maxWidth, int maxHeight) async {
+  static Future<String?> compressImage(String path, int quality, int maxWidth, int maxHeight) async {
     //剔除file部分
     String nowerPath = path;
     //地址
@@ -31,27 +31,27 @@ class Flutterimagecompress {
       nowerPath = nowerPath.replaceAll("file://", "");
     }
     //调用原生压缩图像
-    final String ret = await _channel.invokeMethod('compressImage',
+    final String? ret = await _channel.invokeMethod('compressImage',
         {"path": nowerPath, "savePath": "", "quality": quality.toString(), "maxWidth": maxWidth.toString(), "maxHeight": maxHeight.toString()});
     return ret;
   }
 
   //获取默认的压缩地址
-  static Future<String> getCompressDefaultPath() async {
-    final String ret = await _channel.invokeMethod('getCompressDefaultPath', {});
+  static Future<String?> getCompressDefaultPath() async {
+    final String? ret = await _channel.invokeMethod('getCompressDefaultPath', {});
     return ret;
   }
 
   //获取应用默认临时缓存地址
-  static Future<String> getCacheDefaultPath() async {
-    final String ret = await _channel.invokeMethod('getCacheDefaultPath', {});
+  static Future<String?> getCacheDefaultPath() async {
+    final String? ret = await _channel.invokeMethod('getCacheDefaultPath', {});
     return ret;
   }
 
   //保存图片
-  static Future<String> saveImage(Uint8List imageData, String savePath, String imageName) async {
+  static Future<String?> saveImage(Uint8List imageData, String savePath, String imageName) async {
     try {
-      String path = await _channel.invokeMethod('saveImage', {
+      String? path = await _channel.invokeMethod('saveImage', {
         "imageData": imageData,
         "imageName": imageName,
         "savePath": savePath,
